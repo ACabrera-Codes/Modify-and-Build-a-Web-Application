952d7fc8e800558cfa670f8b55ad442110eaf8cc
A.C_Codes <62225871+ACabrera-Codes@users.noreply.github.com>
2020-09-19 14:43:30 -0400

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -7,8 +7,8 @@ http.createServer(function (request, response) {
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
-   // Send the response body as "Hello World"
-   response.end('Welcome World!\n')
+   // Send the response body as "Welcome World"
+   response.end('Welcome World\n')
 }).listen(3333)
 
 // Console will print the message
